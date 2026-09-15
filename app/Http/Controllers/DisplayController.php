82324667c8583395d9e74a6a6e4cb9d2dd44c921
Tsamrotul Fuad <tsamrotulfuad@live.com>
2026-09-15 13:39:47 +0700

    add livewire and fix display

--- app/Http/Controllers/DisplayController.php
+++ app/Http/Controllers/DisplayController.php
@@ -2,30 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Agenda;
-use App\Models\Kegiatan;
-use App\Models\Media;
-use Illuminate\Http\Request;
-use Illuminate\Support\Carbon;
-
 class DisplayController extends Controller
 {
     public function index()
     {
-        // Gambar
-        $media = Media::where('status', 'active')->get();
-        
-        // Kegiatan
-        $kegiatan = Kegiatan::whereDate('tanggal_kegiatan', Carbon::today())
-            ->orderBy('waktu_mulai', 'asc')
-            ->get();
-        
-        $agenda = Agenda::where('tanggal_agenda', '>=', Carbon::now()->subDays(7))->get();
-        return view('display',  [
-            'kegiatanHariIni' => $kegiatan,
-            'media' => $media,
-            'agenda' => $agenda,
-            'tanggal' => Carbon::now()->isoFormat('D MMMM Y') // Contoh: 22 Juli 2026
-        ]);
+        return view('display');
     }
 }
